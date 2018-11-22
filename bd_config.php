b545f1bfadafb2eac4695223d95241804faadf39
Felipe Gonzalez Alarcon <Felipe.gonzalezalarcon94@gmail.com>
2018-11-22 15:28:29 -0300

    Actualizacion repositorio

--- bd_config.php
+++ bd_config.php
@@ -4,20 +4,21 @@
 	BD config conexion
  */
 
-function connect(){
-	$servername="mysql1";
+function connect_pdo(){
+	$servername ="localhost";
 	$username="lastquake";
 	$password="rokita6195236";
 	$bd="lastquake";
 
-	$conn = new mysqli($servername,$username,$password,$bd);
-
-	if($conn->connect_error){
-		die("Connection failed: ".$conn->connect_error);
+	try{
+		$conn = new PDO("mysql:host=$servername;dbname=$bd",$username,$password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+		$conn -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+		echo "Connected Succesfully<br>";
+		return $conn;
 	}
-
-	return $conn;
-	
-
+	catch(PDOException $e){
+		echo "Connection failed: ".$e->getMessage();
+	}
 }
+
 ?>
