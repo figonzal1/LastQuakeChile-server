3ecbcca4ed62fb5e4cce3a6e3fcb2cba8a11d022
figonzal <felipe.gonzalezal.13@sansano.usm.cl>
2019-01-07 23:26:41 -0300

    testing

--- configs/send_notification.php
+++ configs/send_notification.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once "../vendor/autoload.php";
+require_once ".:/app/.heroku/php/lib/php/../vendor/autoload.php";
 use Kreait\Firebase\Factory;
 use Kreait\Firebase\ServiceAccount;
 use Kreait\Firebase\Messaging\CloudMessage;
