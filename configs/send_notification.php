c70703d5fddef115aa1381dbcf1af7094874f97e
figonzal <felipe.gonzalezal.13@sansano.usm.cl>
2019-03-11 21:59:22 -0300

    Limpieza de codigo y eliminacion de duplicaciones en send_notification

--- configs/send_notification.php
+++ configs/send_notification.php
@@ -8,11 +8,26 @@ use Kreait\Firebase\Messaging\MessageToRegistrationToken;
 use Kreait\Firebase\Messaging\AndroidConfig;
 use Kreait\Firebase\Exception\Messaging\InvalidMessage;
 
+/**
+ * [sendNotification description]
+ * Funcion encargada de enviar notificaciones FCM a los dispositivos suscritos al canal 'Quakes'
+ * @param  [type] $prefijo     Prefijo utilizado para el titulo de la notificacion
+ * @param  [type] $fecha_utc   FEcha UTC del sismos
+ * @param  [type] $latitud     Latitud de sismo
+ * @param  [type] $longitud    Longitud del sismo
+ * @param  [type] $profundidad Profundidad en km del sismo
+ * @param  [type] $magnitud    Magnitud del sismo
+ * @param  [type] $escala      Escala en Mw o Ml del sismo
+ * @param  [type] $sensible    Sismo sensible por la comunidad
+ * @param  [type] $referencia  Referencia georafica del sismo (x km del x ciudad)
+ * @param  [type] $imagen      Link (url) de la imagen desde sismologia.cl
+ * @param  [type] $estado      Sismo preliminar o sismo verificado
+ * @return [type]              S/D
+ */
+function sendNotification($tipo_mensaje,$prefijo,$fecha_utc,$latitud,$longitud,$profundidad,$magnitud,$escala,$sensible,$referencia,$imagen,$estado){
 
-
-function sendNotification($prefijo,$fecha_utc,$latitud,$longitud,$profundidad,$magnitud,$escala,$sensible,$referencia,$imagen,$estado){
-
-	
+	//Revisar token de dispositivo en android
+	$deviceToken="dgto7240DZw:APA91bHxHhU8yDUkmdaB4XJbVGz1hIc6hFfGWGIyiUn6l0T8Nbl7SYxg9-fLAZ7jzraH8gsrB1OwZhSEk4iGnbTUlaJg9IdCQtBVQlp-6Txco0Og_4-mQybrBQfB6eki_HGTQBGksBrX";
 	$serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/lastquake_credentials.json');
 	
 	$firebase = (new Factory)
@@ -21,61 +36,42 @@ function sendNotification($prefijo,$fecha_utc,$latitud,$longitud,$profundidad,$m
 
 	$messaging = $firebase->getMessaging();
 
-
-
 	if ($estado=="preliminar") {
 
-			//Configuracion mensaje ANDROID
+		//Configuracion mensaje ANDROID
 		$config = AndroidConfig::fromArray([
 				'ttl' => '1200s',   // 20 minutos de expiracion para sismo preliminar
 				'priority' => 'high'  //Prioridad HIGH
 			]);
-		$data=[
-			'titulo' => '[Preliminar] ¡Alerta sísmica!',
-			'descripcion' => 'Sismo de '.$magnitud.' registrado a '.$referencia,
-			'latitud' => $latitud,
-			'longitud' => $longitud,
-			'fecha_utc' => $fecha_utc,
-			'magnitud' => $magnitud,
-			'escala' => $escala,
-			'profundidad' => $profundidad,
-			'sensible' => $sensible,
-			'referencia' => $referencia,
-			'imagen_url' => $imagen,
-			'estado' => $estado
-		];
+		$data=setNotificationData('[Preliminar] ',$fecha_utc,$latitud,$longitud,$profundidad,$magnitud,$escala,$sensible,$referencia,$imagen,$estado);
+
 	}
 		//Sismo verificado o sismo corregido (prefijo)
 	else if ($estado=="verificado") {
 
-			//Configuracion mensaje ANDROID
+		//Configuracion mensaje ANDROID
 		$config = AndroidConfig::fromArray([
 				'ttl' => '3600s',   // 1 Hora de expiracion para sismo verificado
 				'priority' => 'high'  //Prioridad HIGH
 			]);
 
-		$data=[
-			'titulo' => $prefijo.'¡Alerta sísmica!',
-			'descripcion' => 'Sismo de '.$magnitud.' registrado a '.$referencia,
-			'latitud' => $latitud,
-			'longitud' => $longitud,
-			'fecha_utc' => $fecha_utc,
-			'magnitud' => $magnitud,
-			'escala' => $escala,
-			'profundidad' => $profundidad,
-			'sensible' => $sensible,
-			'referencia' => $referencia,
-			'imagen_url' => $imagen,
-			'estado' => $estado
-		];
+		$data=setNotificationData($prefijo,$fecha_utc,$latitud,$longitud,$profundidad,$magnitud,$escala,$sensible,$referencia,$imagen,$estado);
 	}
 
+	//Envia solo a un dispositivo
+	if ($tipo_mensaje=="Test") {
+		$message = CloudMessage::withTarget('token',$deviceToken)
+		->withAndroidConfig($config)
+		->withData($data);
+	}
 
-	$topic='Quakes';
-
-	$message = CloudMessage::withTarget('topic',$topic)
-	->withAndroidConfig($config)
-	->withData($data);
+	//Envia a todos los dispositivos en Quakes
+	else if ($tipo_mensaje=="Quakes") {
+		$topic='Quakes';
+		$message = CloudMessage::withTarget('topic',$topic)
+		->withAndroidConfig($config)
+		->withData($data);
+	}
 
 	$response =$messaging->send($message);
 
@@ -90,81 +86,38 @@ function sendNotification($prefijo,$fecha_utc,$latitud,$longitud,$profundidad,$m
 }
 
 
-/*
-function sendNotificationToDevice($prefijo,$fecha_utc,$latitud,$longitud,$profundidad,$magnitud,$escala,$sensible,$referencia,$imagen,$estado){
-
-
-	$deviceToken="dgto7240DZw:APA91bHxHhU8yDUkmdaB4XJbVGz1hIc6hFfGWGIyiUn6l0T8Nbl7SYxg9-fLAZ7jzraH8gsrB1OwZhSEk4iGnbTUlaJg9IdCQtBVQlp-6Txco0Og_4-mQybrBQfB6eki_HGTQBGksBrX";
-	$serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/lastquake_credentials.json');
-
-	$firebase = (new Factory)
-	->withServiceAccount($serviceAccount)
-	->create();
-
-	$messaging = $firebase->getMessaging();
-
-
-
-	if ($estado=="preliminar") {
-
-			//Configuracion mensaje ANDROID
-		$config = AndroidConfig::fromArray([
-				'ttl' => '1200s',   // 20 minutos de expiracion para sismo preliminar
-				'priority' => 'high'  //Prioridad HIGH
-			]);
-		$data=[
-			'titulo' => '[Preliminar] ¡Alerta sísmica!',
-			'descripcion' => 'Sismo de '.$magnitud.' registrado a '.$referencia,
-			'latitud' => $latitud,
-			'longitud' => $longitud,
-			'fecha_utc' => $fecha_utc,
-			'magnitud' => $magnitud,
-			'escala' => $escala,
-			'profundidad' => $profundidad,
-			'sensible' => $sensible,
-			'referencia' => $referencia,
-			'imagen_url' => $imagen,
-			'estado' => $estado
-		];
-	}
-		//Sismo verificado o sismo corregido (prefijo)
-	else if ($estado=="verificado") {
-
-			//Configuracion mensaje ANDROID
-		$config = AndroidConfig::fromArray([
-				'ttl' => '3600s',   // 1 Hora de expiracion para sismo verificado
-				'priority' => 'high'  //Prioridad HIGH
-			]);
-
-		$data=[
-			'titulo' => $prefijo.'¡Alerta sísmica!',
-			'descripcion' => 'Sismo de '.$magnitud.' registrado a '.$referencia,
-			'latitud' => $latitud,
-			'longitud' => $longitud,
-			'fecha_utc' => $fecha_utc,
-			'magnitud' => $magnitud,
-			'escala' => $escala,
-			'profundidad' => $profundidad,
-			'sensible' => $sensible,
-			'referencia' => $referencia,
-			'imagen_url' => $imagen,
-			'estado' => $estado
-		];
-	}
-
-	$message = CloudMessage::withTarget('token',$deviceToken)
-	->withAndroidConfig($config)
-	->withData($data);
-
-	$response =$messaging->send($message);
-
-	echo json_encode($response,JSON_PRETTY_PRINT);
-
-	try {
-		$firebase->getMessaging()->validate($message);
-	} catch (InvalidMessage $e) {
-		print_r($e->errors());
-	}
-}*/
+/**
+ * [setNotificationData description]
+ * Funcion encargada de setear el mensaje de la notifiacacion
+ * @param  [type] $prefijo     Prefijo utilizado para el titulo de la notificacion
+ * @param  [type] $fecha_utc   FEcha UTC del sismos
+ * @param  [type] $latitud     Latitud de sismo
+ * @param  [type] $longitud    Longitud del sismo
+ * @param  [type] $profundidad Profundidad en km del sismo
+ * @param  [type] $magnitud    Magnitud del sismo
+ * @param  [type] $escala      Escala en Mw o Ml del sismo
+ * @param  [type] $sensible    Sismo sensible por la comunidad
+ * @param  [type] $referencia  Referencia georafica del sismo (x km del x ciudad)
+ * @param  [type] $imagen      Link (url) de la imagen desde sismologia.cl
+ * @param  [type] $estado      Sismo preliminar o sismo verificado
+ * @return [type]              S/D
+ */
+function setNotificationData($prefijo,$fecha_utc,$latitud,$longitud,$profundidad,$magnitud,$escala,$sensible,$referencia,$imagen,$estado){
+	
+	return $data=[
+		'titulo' => $prefijo.'¡Alerta sísmica!',
+		'descripcion' => 'Sismo de '.$magnitud.' registrado a '.$referencia,
+		'latitud' => $latitud,
+		'longitud' => $longitud,
+		'fecha_utc' => $fecha_utc,
+		'magnitud' => $magnitud,
+		'escala' => $escala,
+		'profundidad' => $profundidad,
+		'sensible' => $sensible,
+		'referencia' => $referencia,
+		'imagen_url' => $imagen,
+		'estado' => $estado
+	];
+}
 
 ?>
