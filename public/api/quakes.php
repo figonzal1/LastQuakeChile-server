33800a3f410bd56851614207f49c4b93487ed11d
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2019-04-02 17:56:40 -0300

    Implementación API casi terminada

--- public/api/quakes.php
+++ public/api/quakes.php
@@ -4,118 +4,263 @@ header('Content-type: application/json; charset=UTF-8');
 require_once "../../configs/bd_config.php";
 
 
-$sql = "SELECT * FROM quakes";
+if ($_SERVER['REQUEST_METHOD']=='GET'){
 
-/**
- * Procesa solicitudes de fecha con limite y sin limite
- */
-if (isset($_GET['anno']) and isset($_GET['mes']) ) {
-
-    $anno = $_GET['anno'];
-    $mes = $_GET['mes'];
-    $dia = null;
+    $sql = "SELECT * FROM quakes";
     
-    $sql = "SELECT * FROM quakes WHERE Year(fecha_local)=$anno and Month(fecha_local)=$mes";
+    /**
+     * Resuelve peticiones de fechas
+     * procesa solicitudes de fecha con limite y sin limite
+     */
+    if (isset($_GET['anno']) and isset($_GET['mes']) ) {
 
-    $args = array(
-        'anno' => $anno,
-        'mes' => $mes
-    );
+        $anno = $_GET['anno'];
+        $mes = $_GET['mes'];
+        $dia = null;
+        
+        $sql = "SELECT * FROM quakes WHERE Year(fecha_local)=$anno and Month(fecha_local)=$mes";
 
-    if (isset($_GET['dia']) and !empty($_GET['dia'])) {
-        $dia = $_GET['dia'];
-        $sql = $sql." and Day(fecha_local)=$dia";
+        $params = array(
+            'anno' => $anno,
+            'mes' => $mes
+        );
 
-        $args['dia'] = $dia;
+        if (isset($_GET['dia']) and !empty($_GET['dia'])) {
+            $dia = $_GET['dia'];
+            $sql = $sql." and Day(fecha_local)=$dia";
+
+            $params['dia'] = $dia;
+        }
+
+        $sql = $sql." ORDER BY fecha_local DESC";
+
+        //Limite dado por usuario
+        if (isset($_GET['limite']) and !empty($_GET['limite'])){
+
+            $limite = $_GET['limite'];
+
+            if($limite>400){
+                $sql = $sql." LIMIT 400";
+                $params['default_limite'] = '400'; 
+            }else{
+                $sql = $sql." LIMIT $limite";
+                $params['limite'] = $limite;
+            }
+            
+
+        }else{
+            //Limite por defecto
+            $sql= $sql." LIMIT 400";
+            $params['default_limite'] = '400';
+        }
+        
+
+        $quakes = doQuery($sql);
+        echo json_encode(
+            array(
+                'parametros' => $params,
+                'sismos' => $quakes
+            ),
+            JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
+        );
     }
 
-    $sql = $sql." ORDER BY fecha_local DESC";
 
-    //Limite dado por usuario
-    if (isset($_GET['limite']) and !empty($_GET['limite'])){
+    /**
+     * Resuelve peticiones de ciudades con y sin limite
+     */
+    else if(isset($_GET['ciudad'])and !empty($_GET['ciudad'])){
 
+        $ciudad = $_GET['ciudad'];
+        $sql = $sql." WHERE referencia LIKE '%$ciudad%'";
+
+        $params['ciudad']=$ciudad;
+
+        //NO VALE LA PENA ARGUMENTO MAGNITUD CON CIUDAD
+        /*if (isset($_GET['magnitud']) and !empty($_GET['magnitud'])){
+            $magnitud= $_GET['magnitud'];
+            $params['magnitud']=$magnitud;
+            $sql = $sql." AND magnitud=$magnitud";
+        }*/
+
+        $sql = $sql." ORDER BY fecha_local DESC";
+
+        //Limite dado por usuario
+        if (isset($_GET['limite']) and !empty($_GET['limite'])){
+
+            $limite = $_GET['limite'];
+
+            if($limite>400){
+                $sql = $sql." LIMIT 400";
+                $params['default_limite'] = '400'; 
+            }else{
+                $sql = $sql." LIMIT $limite";
+                $params['limite'] = $limite;
+            }
+
+        }else{
+            //Limite por defecto
+            $sql= $sql." LIMIT 400";
+            $params['default_limite'] = '400';
+        }
+
+        $quakes = doQuery($sql);
+        echo json_encode(
+            array(
+                'parametros' => $params,
+                'sismos' => $quakes
+            ),
+            JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
+        );
+    }
+
+    /**
+     * Resuelve peticiones de magnitud
+     */
+    else if (isset($_GET['magnitud']) and !empty($_GET['magnitud'])){
+
+        $magnitud = $_GET['magnitud'];
+        $sql = $sql." WHERE magnitud=$magnitud";
+
+        $params['magnitud']=$magnitud;
+
+        $sql = $sql." ORDER BY fecha_local DESC";
+        
+        //Limite dado por usuario
+        if (isset($_GET['limite']) and !empty($_GET['limite'])){
+
+            $limite = $_GET['limite'];
+
+            if($limite>400){
+                $sql = $sql." LIMIT 400";
+                $params['default_limite'] = '400'; 
+            }else{
+                $sql = $sql." LIMIT $limite";
+                $params['limite'] = $limite;
+            }
+
+        }else{
+            //Limite por defecto
+            $sql= $sql." LIMIT 400";
+            $params['default_limite'] = '400';
+        }
+
+        $quakes = doQuery($sql);
+        echo json_encode(
+            array(
+                'parametros' => $params,
+                'sismos' => $quakes
+            ),
+            JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
+        );
+    }
+
+    /**
+     * Resuelve peticiones de top ranking
+     */
+    else if (isset($_GET['ranking']) and !empty($_GET['ranking'])){
+
+        $ranking = $_GET['ranking'];
+
+        if ($ranking=='magnitud'){
+            $sql = $sql." ORDER BY magnitud DESC";
+            $params['ranking']=$ranking;
+        }
+        else if ($ranking=='profundidad'){
+            $sql= $sql." ORDER BY profundidad DESC";
+            $params['ranking']=$ranking;
+        }
+        else{
+            http_response_code(400);
+            exit();
+        }
+
+        //Limite dado por usuario
+        if (isset($_GET['limite']) and !empty($_GET['limite'])){
+
+            $limite = $_GET['limite'];
+
+            if($limite>400){
+                $sql = $sql." LIMIT 400";
+                $params['default_limite'] = '400'; 
+            }else{
+                $sql = $sql." LIMIT $limite";
+                $params['limite'] = $limite;
+            }
+
+        }else{
+            //Limite por defecto
+            $sql= $sql." LIMIT 400";
+            $params['default_limite'] = '400';
+        }
+
+        $quakes = doQuery($sql);
+        echo json_encode(
+            array(
+                'parametros' => $params,
+                'sismos' => $quakes
+            ),
+            JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
+        );
+        
+    }
+
+    /**
+     * Procesa solo limites de registros
+     */
+    else if (isset($_GET['limite']) and !empty($_GET['limite'])){
         $limite = $_GET['limite'];
-        $sql = $sql." LIMIT $limite";
-        $args['limite'] = $limite;
 
-    }else{
-        //Limite por defecto
-        $sql= $sql." LIMIT 400";
-        $args['default_limite'] = 400;
-    }
+        $sql = $sql." ORDER BY fecha_local DESC";
+        $limite = $_GET['limite'];
     
+        if($limite>=400){
+            $sql = $sql." LIMIT 400";
+            $params['default_limite'] = '400'; 
+        }else{
+            $sql = $sql." LIMIT $limite";
+            $params['limite'] = $limite;
+        }
 
-    $quakes = doQuery($sql);
-    echo json_encode(
-        array(
-            'http-code' => 200,
-            'parameters' => $args,
-            'quakes' => $quakes
-        ),
-        JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
-    );
-}
-
-/**
- * Resuelve peticiones de ciudades
- */
-else if(isset($_GET['ciudad'])and !empty($_GET['ciudad'])){
-
-    $ciudad = $_GET['ciudad'];
-    $sql = $sql." WHERE referencia LIKE '%$ciudad%'";
-
-    $args['ciudad']=$ciudad;
-
-    $sql = $sql." ORDER BY fecha_local DESC";
-
-    //Limite dado por usuario
-    if (isset($_GET['limite']) and !empty($_GET['limite'])){
-
-        $limite = $_GET['limite'];
-        $sql = $sql." LIMIT $limite";
-        $args['limite'] = $limite;
-
-    }else{
-        //Limite por defecto
-        $sql= $sql." LIMIT 400";
-        $args['default_limite'] = 400;
+        $quakes = doQuery($sql);
+        echo json_encode(
+            array(
+                'parametros' => $params,
+                'sismos' => $quakes
+            ),
+            JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
+        );
     }
 
-    $quakes = doQuery($sql);
-    echo json_encode(
-        array(
-            'http-code' => 200,
-            'parameters' => $args,
-            'quakes' => $quakes
-        ),
-        JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
-    );
+    //Si no user no entrega opciones, 400 limite por defecto
+    else if (!isset($_GET['limite'])){
+        $sql = $sql." ORDER BY fecha_local DESC LIMIT 400";
+        $params['default_limite']='400';
+
+        $quakes = doQuery($sql);
+        echo json_encode(
+            array(
+                'parametros' => $params,
+                'sismos' => $quakes
+            ),
+            JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
+        );
+    }
+
+    //URL mal escrita error 400
+    else{
+        http_response_code(400);
+        exit();
+    }
 }
 
-/**
- * Procesa solo limtes
- */
-else if (isset($_GET['limite']) and !empty($_GET['limite'])){
-    $limite = $_GET['limite'];
-
-    $sql = $sql." ORDER BY fecha_local DESC LIMIT $limite";
-    $args['limite']=$limite;
-
-    $quakes = doQuery($sql);
-    echo json_encode(
-        array(
-            'http-code' => 200,
-            'parameters' => $args,
-            'quakes' => $quakes
-        ),
-        JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
-    );
-}
-
+//Si no es get , error 405
 else{
-    http_response_code(404);
+    http_response_code(501);
     exit();
 }
+
+
 
 
 function doQuery($sql)
