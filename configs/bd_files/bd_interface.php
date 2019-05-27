1fee76445a3ab83b98696a48f04a755b488c3e88
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2019-05-27 15:55:30 -0400

    Cambio de parametro en funcion find

--- configs/bd_files/bd_interface.php
+++ configs/bd_files/bd_interface.php
@@ -2,7 +2,7 @@
 
 interface BdAdapter{
     public function addQuake($quake);
-    public function findQuake($imagen);
+    public function findQuake($quake);
     public function updateQuake($quake);
 }
 ?>
