81bf586595bfaa7db5ac8792349ab8bd7b6f29e9
Felipe Gonzalez Alarcon <Felipe.gonzalezalarcon94@gmail.com>
2021-01-23 22:04:00 -0300

    Test

--- src/configs/send_notification.php
+++ src/configs/send_notification.php
@@ -29,10 +29,10 @@ function sendNotification($tipo_mensaje, $prefijo, $fecha_utc, $ciudad, $latitud
 {
 
 	//Revisar token de dispositivo en android
-	//$deviceToken = '';
+	#$deviceToken = '';
 	
-	$factory = (new Factory())->withServiceAccount(getenv("FIREBASE_CREDENTIALS"));
-	//$factory = (new Factory())->withServiceAccount("absolute/path");
+	##$factory = (new Factory())->withServiceAccount(getenv("FIREBASE_CREDENTIALS"));
+	$factory = (new Factory())->withServiceAccount("C:\\xampp\\htdocs\\LastQuakeChile-server\\lqch-server-credentials.json");
 
 	$messaging = $factory->createMessaging();
 
